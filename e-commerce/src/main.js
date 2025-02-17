bca30269c0fa4fe5e5143eca021336d1d6fed899
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-02-17 20:34:08 +0400

    fix: some responsivity issues

--- e-commerce/src/main.js
+++ e-commerce/src/main.js
@@ -8,18 +8,43 @@ const tl = gsap.timeline({repeat: -1})
 
 gsap.from(".logo-animation",{
     fill: "transparent",
-    duration: 0.5
+    duration: 0.5,
+    y:100
+})
+
+
+gsap.utils.toArray(document.querySelectorAll(".nav-list")).forEach((link)=>{
+  gsap.from(link,{
+    opacity:0,
+    y:100,
+    duration:0.5,
+  })
+})
+
+gsap.from(".banner",{
+  opacity:0,
+  duration:1
+})
+gsap.from(".close img",{
+  x:"100vw",
+  duration:0.3,
+})
+
+gsap.from(".logos svg",{
+  y:150,
+  duration:1,
 })
 
 
 gsap.to(".title-animation", {
-    duration: 1,
+    duration: 0.5,
     text: {
       value: "FIND CLOTHES THAT MATCHES",
       newClass: "",
       delimiter: "",
     },
   })
+
   
   
   tl.to(".infinite-animation",{
